61effbf16ef2a02889f50cb46981337c055955f2
ximi1201 <167153869+ximi1201@users.noreply.github.com>
2024-05-26 10:05:15 -0400

    added search function using API

--- src/script.js
+++ src/script.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#City");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b0af3ae16bb5t0d34c6o8137296984ee";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#searchInput");
-  let cityElement = document.querySelector("#City");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#searchForm");
